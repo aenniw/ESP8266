f3902fb00c5af412ab0ca32a584248b43f41b0ed
Martin Mihálek <aenniw@gmail.com>
2017-03-18 20:27:51 +0100

    Add html compresion
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

---
+++ IOTbase/extra_script.py
@@ -0,0 +1,49 @@
+import gzip
+import os
+import shutil
+
+Import("env")
+
+
+def cleanup_dir(directory):
+    for root, dirs, files in os.walk(directory):
+        for dir_to_proccess in dirs:
+            if not dir_to_proccess.startswith("."):
+                cleanup_dir(root + "/" + dir_to_proccess)
+                os.rmdir(root + "/" + dir_to_proccess)
+        for name in files:
+            os.remove(root + "/" + name)
+
+
+def process_dir(directory):
+    for root, dirs, files in os.walk(directory):
+        dest_root = "./data/" + "".join(root.split("/")[2:])
+        for name in files:
+            if name.endswith((".html", ".css", ".js")):
+                with open(root + "/" + name, 'rb') as f_in, gzip.open(dest_root + "/" + name + ".gz", 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+            elif name.endswith(".json"):
+                with open(root + "/" + name, 'r') as f_in, open(dest_root + "/" + name, 'w') as f_out:
+                    while True:
+                        c = f_in.read(1)
+                        if not c:
+                            break
+                        elif c == " " or c == "\t" or c == "\r" or c == "\n":
+                            continue
+                        f_out.write(c)
+            else:
+                shutil.copyfile(root + "/" + name, dest_root + "/" + name)
+        for dir_to_process in dirs:
+            if not dir_to_process.startswith("."):
+                os.mkdir(dest_root + "/" + dir_to_process)
+                process_dir(root + "/" + dir_to_process)
+
+
+def compress_html_resources():
+    print "Cleanup of resources for FS"
+    cleanup_dir("./data")
+    print "Building resources for FS"
+    process_dir("./resources")
+
+if "uploadfs" in BUILD_TARGETS or "buildfs" in BUILD_TARGETS:
+    compress_html_resources()
--- lib/logger/logger.cpp
+++ lib/logger/logger.cpp
@@ -8,13 +8,51 @@ void Log::init() {
 #endif
 }
 
+void Log::print(const char *format, ...) {
+#ifdef __DEBUG__
+    va_list arg;
+    va_start(arg, format);
+    char temp[64];
+    char *buffer = temp;
+    size_t len = vsnprintf(temp, sizeof(temp), format, arg);
+    va_end(arg);
+    if (len > sizeof(temp) - 1) {
+        buffer = new char[len + 1];
+        if (!buffer) {
+            return;
+        }
+        va_start(arg, format);
+        vsnprintf(buffer, len + 1, format, arg);
+        va_end(arg);
+    }
+    Serial.print(buffer);
+    if (buffer != temp) {
+        delete[] buffer;
+    }
+#endif
+}
+
 void Log::println(const char *format, ...) {
 #ifdef __DEBUG__
-    va_list args;
-    va_start(args, format);
-    Serial.printf(format, args);
-    Serial.println();
-    va_end(args);
+    va_list arg;
+    va_start(arg, format);
+    char temp[64];
+    char *buffer = temp;
+    size_t len = vsnprintf(temp, sizeof(temp), format, arg);
+    va_end(arg);
+    if (len > sizeof(temp) - 1) {
+        buffer = new char[len + 1];
+        if (!buffer) {
+            return;
+        }
+        va_start(arg, format);
+        vsnprintf(buffer, len + 1, format, arg);
+        va_end(arg);
+    }
+    Serial.println(buffer);
+    if (buffer != temp) {
+        delete[] buffer;
+    }
 #endif
 }
 
