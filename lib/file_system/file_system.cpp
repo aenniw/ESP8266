34afa884e5176c5a01fc86853e670cef88b098bf
Martin Mihálek <aenniw@gmail.com>
2017-04-23 13:20:16 +0200

    Update lib file_system
    - Reworked handling of JSON config files
    
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

--- lib/file_system/file_system.cpp
+++ lib/file_system/file_system.cpp
@@ -1,12 +1,48 @@
 #include <file_system.h>
 
-String get_file_content(const char *file_name) {
+String ICACHE_FLASH_ATTR get_file_content(const char *file_name) {
     File file = SPIFFS.open(file_name, "r");
     if (!file) {
-        Serial.println("Failed to open config file");
+        Serial.println("Failed to open file");
         return "";
     }
     String data = file.readString();
     file.close();
     return data;
+}
+
+char * ICACHE_FLASH_ATTR ConfigJSON::get_string(JsonObject &json, std::initializer_list<const char *> *keys,
+                                    std::initializer_list<const char *>::const_iterator i) {
+    if (!json.containsKey(*i)) {
+        return NULL;
+    } else if (keys->end() == i + 1) {
+        if (json[*i].is<const char *>()) {
+            const char *stack = json[*i].as<const char *>();
+            char *heap = (char *) malloc(sizeof(char) * (strlen(stack) + 1));
+            strcpy(heap, stack);
+            return heap;
+        } else {
+            return NULL;
+        }
+    } else {
+        return get_string(json[*i].as<JsonObject>(), keys, i + 1);
+    }
+}
+
+char *ConfigJSON::getString(const char *file, std::initializer_list<const char *> keys) {
+    File configFile = SPIFFS.open(file, "r");
+    if (!configFile) {
+        Log::println("No file");
+        return NULL;
+    }
+    std::unique_ptr<char[]> buf(new char[configFile.size()]);
+    configFile.readBytes(buf.get(), configFile.size());
+    configFile.close();
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject &json = jsonBuffer.parseObject(buf.get());
+    if (!json.success()) {
+        return NULL;
+    } else {
+        return get_string(json, &keys, keys.begin());
+    }
 }
--- lib/file_system/file_system.h
+++ lib/file_system/file_system.h
@@ -1,88 +1,82 @@
 #ifndef WEMOS_D1_FILESYSTEM_H_
 #define WEMOS_D1_FILESYSTEM_H_
 
+#include <logger.h>
 #include <ArduinoJson.h>
 #include <initializer_list>
 #include <FS.h>
 
 String ICACHE_FLASH_ATTR get_file_content(const char *);
 
-template<typename T>
-T get_config_(JsonObject &json, std::initializer_list<const char *> *keys,
-              std::initializer_list<const char *>::const_iterator i) {
-    if (!json.containsKey(*i)) {
-        return 0;
-    } else if (keys->end() == i + 1) {
-        return json[*i].is<T>() ? json[*i].as<T>() : 0;
-    } else {
-        return get_config_<T>(json[*i].asObject(), keys, i + 1);
-    }
-}
-
-template<>
-const char *get_config_(JsonObject &json, std::initializer_list<const char *> *keys,
-                        std::initializer_list<const char *>::const_iterator i) {
-    if (!json.containsKey(*i)) {
-        return NULL;
-    } else if (keys->end() == i + 1) {
-        if (!json[*i].is<const char *>())
-            return NULL;
-        const char *stack_array = json[*i].asString();
-        char *heap_array = (char *) malloc(sizeof(char) * (strlen(stack_array) + 1));
-        strcpy(heap_array, stack_array);
-        return heap_array;
-    } else {
-        return get_config_<const char *>(json[*i].asObject(), keys, i + 1);
-    }
-}
-
-template<typename T>
-T get_config(const char *file, std::initializer_list<const char *> keys) {
-    File configFile = SPIFFS.open(file, "r");
-    if (!configFile) {
-        Serial.println("No file");
-        return std::is_pointer<T>::value ? NULL : 0;
-    }
-    std::unique_ptr<char[]> buf(new char[configFile.size()]);
-    configFile.readBytes(buf.get(), configFile.size());
-    configFile.close();
-    DynamicJsonBuffer jsonBuffer;
-    JsonObject &json = jsonBuffer.parseObject(buf.get());
-    if (!json.success()) {
-        return std::is_pointer<T>::value ? NULL : 0;
-    } else {
-        return get_config_<T>(json, &keys, keys.begin());
-    }
-}
-
-template<typename T>
-void set_config_(JsonObject &json, std::initializer_list<const char *> *keys,
-                 std::initializer_list<const char *>::const_iterator i, T *value) {
-    if (keys->end() == i + 1) {
-        json[*i] = *value;
-    } else {
+class ConfigJSON {
+private:
+    template<typename T>
+    static T ICACHE_FLASH_ATTR get_default(JsonObject &json, std::initializer_list<const char *> *keys,
+                                           std::initializer_list<const char *>::const_iterator i) {
         if (!json.containsKey(*i)) {
-            json.createNestedObject(*i);
+            return 0;
+        } else if (keys->end() == i + 1) {
+            return json[*i].is<T>() ? json[*i].as<T>() : 0;
         } else {
-            set_config_<T>(json[*i].asObject(), keys, i + 1, value);
+            return get_default<T>(json[*i].as<JsonObject>(), keys, i + 1);
         }
     }
-}
 
-template<typename T>
-void set_config(const char *file, std::initializer_list<const char *> keys, T value) {
-    File configFile = SPIFFS.open(file, "r");
-    if (!configFile) return;
-    std::unique_ptr<char[]> buf(new char[configFile.size()]);
-    configFile.readBytes(buf.get(), configFile.size());
-    DynamicJsonBuffer jsonBuffer;
-    JsonObject &json = jsonBuffer.parseObject(buf.get());
-    configFile.close();
-    configFile = SPIFFS.open(file, "w");
-    if (!configFile) return;
-    set_config_(json, &keys, keys.begin(), &value);
-    json.printTo(configFile);
-    configFile.close();
-}
+    template<typename T>
+    static void ICACHE_FLASH_ATTR set_default(JsonObject &json, std::initializer_list<const char *> *keys,
+                                              std::initializer_list<const char *>::const_iterator i, T *value) {
+        if (keys->end() == i + 1) {
+            json[*i] = *value;
+        } else {
+            if (!json.containsKey(*i)) {
+                json.createNestedObject(*i);
+            } else {
+                set_default<T>(json[*i].as<JsonObject>(), keys, i + 1, value);
+            }
+        }
+    }
+
+    static char *ICACHE_FLASH_ATTR get_string(JsonObject &json, std::initializer_list<const char *> *keys,
+                                              std::initializer_list<const char *>::const_iterator i);
+
+public:
+    template<typename T>
+    static T ICACHE_FLASH_ATTR get(const char *file, std::initializer_list<const char *> keys) {
+        File configFile = SPIFFS.open(file, "r");
+        if (!configFile) {
+            Log::println("No file");
+            return 0;
+        }
+        std::unique_ptr<char[]> buf(new char[configFile.size()]);
+        configFile.readBytes(buf.get(), configFile.size());
+        configFile.close();
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject &json = jsonBuffer.parseObject(buf.get());
+        if (!json.success()) {
+            return 0;
+        } else {
+            return get_default<T>(json, &keys, keys.begin());
+        }
+    }
+
+    template<typename T>
+    static bool ICACHE_FLASH_ATTR set(const char *file, std::initializer_list<const char *> keys, T value) {
+        File configFile = SPIFFS.open(file, "r");
+        if (!configFile) return false;
+        std::unique_ptr<char[]> buf(new char[configFile.size()]);
+        configFile.readBytes(buf.get(), configFile.size());
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject &json = jsonBuffer.parseObject(buf.get());
+        configFile.close();
+        configFile = SPIFFS.open(file, "w");
+        if (!configFile) return false;
+        set_default(json, &keys, keys.begin(), &value);
+        json.printTo(configFile);
+        configFile.close();
+        return true;
+    }
+
+    static char *getString(const char *file, std::initializer_list<const char *> keys);
+};
 
 #endif /* WEMOS_D1_FILESYSTEM_H_ */
