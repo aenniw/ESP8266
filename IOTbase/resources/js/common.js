59a4cef47d16f33823956da57a8157d652868932
Martin Mihálek <aenniw@gmail.com>
2017-04-23 13:22:33 +0200

    Update IOTbase resources
    - Spliting common.js to multiple small js
    - Onload subpage is updated async after timeout
    
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

--- IOTbase/resources/js/common.js
+++ IOTbase/resources/js/common.js
@@ -1,17 +1,6 @@
 var pageCashe = new Map(),
     serviceOnLoad = new Map();
 
-serviceOnLoad.set("status", function statusLoad() {
-    getSystemInfo();
-    getCpuInfo();
-    getMemInfo();
-});
-serviceOnLoad.set("administration", function adminLoad() {
-    getGlobalInfo();
-    getWifiInfo();
-});
-
-
 function loadResource(id, service, loadJs) {
     if (pageCashe.get(service) != null) {
         getE(id).innerHTML = pageCashe.get(service);
@@ -28,7 +17,11 @@ function loadResource(id, service, loadJs) {
             if (req.readyState == 4 && req.status == 200) {
                 pageCashe.set(service, req.responseText);
                 getE(id).innerHTML = req.responseText;
-                if (serviceOnLoad.get(service) != null) serviceOnLoad.get(service)();
+                setTimeout(function () {
+                    if (serviceOnLoad.get(service) != null) {
+                        serviceOnLoad.get(service)();
+                    }
+                }, 200);
             }
         };
         req.send();
@@ -93,97 +86,4 @@ function CORSRequest(method, path) {
         }
     };
     return xhr;
-}
-
-
-function getGlobalInfo() {
-    var req = CORSRequest("GET", "get-config-global");
-    req.onreadystatechange = function () {
-        if (req.readyState == 4 && req.status == 200) {
-            var resp = JSON.parse(req.responseText);
-            set("user", resp["acc"]);
-            set("pass", resp["pass"]);
-        }
-    };
-    req.send();
-}
-
-function getNetworksInfo() {
-    var req = CORSRequest("GET", "get-networks");
-    req.onreadystatechange = function () {
-        if (req.readyState == 4 && req.status == 200) {
-            var resp = JSON.parse(req.responseText);
-            getE("sta-ssid").innerHTML = "";
-            for (var i in resp["networks"]) {
-                getE("sta-ssid").innerHTML += "<option value=" + i + ">" + resp["networks"][i] + "</option>\n";
-            }
-        }
-    };
-    req.send();
-}
-
-function getWifiInfo() {
-    var req = CORSRequest("GET", "get-config-wifi");
-    req.onreadystatechange = function () {
-        if (req.readyState == 4 && req.status == 200) {
-            var resp = JSON.parse(req.responseText);
-            getE("mac").innerHTML = resp["mac"];
-            getE("sta-status").innerHTML = (resp["sta-status"] == 1 ? "Connected" : "Disconnected");
-            getE("sta-ssid").innerHTML = "<option value=\"0\">" + resp["sta-ssid"] + "</option>";
-            getE("sta-ip").innerHTML = resp["sta-ip"];
-            getE("ap-ip").innerHTML = resp["ap-ip"];
-            set("wifi-mode", resp["wifi-mode"]);
-            set("ap-ssid", resp["ap-ssid"]);
-            set("ap-channel", resp["ap-channel"]);
-            set("ap-hidden", resp["ap-hidden"]);
-            set("ap-auth-mode", resp["ap-auth-mode"]);
-            set("hostname", resp["hostname"]);
-        }
-    };
-    req.send();
-}
-
-
-function getCpuInfo() {
-    var req = CORSRequest("GET", "get-cpu-info");
-    req.onreadystatechange = function () {
-        if (req.readyState == 4 && req.status == 200) {
-            var resp = JSON.parse(req.responseText),
-                cur = resp["cpu-freq-cur"], max = resp["cpu-freq-max"];
-            getE("info-cpu").innerHTML = cur + "Mhz / " + max + "Mhz";
-            getE("info-cpu-p").style.width = Math.round((cur / max ) * 100) + "%";
-        }
-    };
-    req.send();
-}
-
-function getSystemInfo() {
-    var req = CORSRequest("GET", "get-system-info");
-    req.onreadystatechange = function () {
-        if (req.readyState == 4 && req.status == 200) {
-            var resp = JSON.parse(req.responseText);
-            getE("model").innerHTML = resp["model"];
-            getE("firmware").innerHTML = resp["firmware"];
-            getE("chip-id").innerHTML = resp["chip-id"];
-        }
-    };
-    req.send();
-}
-
-function getMemInfo() {
-    var req = CORSRequest("GET", "get-mem-info");
-    req.onreadystatechange = function () {
-        if (req.readyState == 4 && req.status == 200) {
-            var resp = JSON.parse(req.responseText),
-                sketchCur = resp["sketch-mem-free"],
-                sketchMax = resp["sketch-mem-total"],
-                memCur = resp["heap-free"],
-                memMax = resp["heap-total"];
-            getE("sketch-info").innerHTML = (sketchCur / 1000) + " kB / " + (sketchMax / 1000) + " kB";
-            getE("sketch-info-p").style.width = Math.round((sketchCur / sketchMax) * 100) + "%";
-            getE("mem-info").innerHTML = (memCur / 1000) + " kB / " + (memMax / 1000) + " kB";
-            getE("mem-info-p").style.width = Math.round((memCur / memMax) * 100) + "%";
-        }
-    };
-    req.send();
 }
