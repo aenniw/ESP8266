006f588a83a25b719964b6c5fd16b568f458c230
Martin Mihálek <aenniw@gmail.com>
2017-04-30 17:50:46 +0200

    Add Device handling component
    - Devices component creates and verifies Devices based on defined pins
    
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

--- lib/devices/devices.cpp
+++ lib/devices/devices.cpp
@@ -1,5 +1,65 @@
 #include "devices.h"
 
+void Devices::get_devices(const DEVICE_TYPE t, std::list<Device *> *d) {
+    if (d == NULL) {
+        return;
+    }
+    for (std::map<uint8_t, Device *>::iterator i = devices.begin();
+         i != devices.end(); i++) {
+        if (t == ANY || (*i).second->get_type() == t) {
+            d->push_back((*i).second);
+        }
+    }
+}
+
+const uint8_t *Devices::get_pins(size_t *len) {
+    (*len) = BOARD_PINS_LEN;
+    return BOARD_PINS;
+}
+
+const bool Devices::valid_pin(const uint8_t p) {
+    for (uint32_t i = 0; i < BOARD_PINS_LEN; i++) {
+        if (p == BOARD_PINS[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Device *Devices::put(const uint8_t p, const DEVICE_TYPE t) {
+    if (!Devices::valid_pin(p) || Devices::get(p) != NULL) {
+        return NULL;
+    }
+    Device *d = NULL;
+    switch (t) {
+        case RELAY:
+            d = new Relay(p);
+            break;
+        default:
+            return NULL;
+    }
+    devices[p] = d;
+    return d;
+}
+
+bool Devices::remove(const uint8_t p) {
+    const Device *d = get(p);
+    if (d == NULL) {
+        return false;
+    }
+    delete d;
+    devices.erase(p);
+    return true;
+}
+
+Device *Devices::get(const uint8_t p) {
+    const std::map<uint8_t, Device *>::iterator iter = devices.find(p);
+    if (iter == devices.end()) {
+        return NULL;
+    }
+    return devices[p];
+}
+
 Relay::Relay(const uint8_t p) {
     pin = p;
     pinMode(pin, OUTPUT);
@@ -16,4 +76,8 @@ void Relay::set_state(const bool flag) {
 
 uint8_t Relay::get_id() {
     return pin;
+}
+
+DEVICE_TYPE Relay::get_type() {
+    return RELAY;
 }
--- lib/devices/devices.h
+++ lib/devices/devices.h
@@ -1,12 +1,42 @@
 #ifndef ESP8266_PROJECTS_ROOT_DEVICES_H
 #define ESP8266_PROJECTS_ROOT_DEVICES_H
 
+#include <map>
+#include <list>
 #include <Arduino.h>
 #include <commons.h>
+
+class Devices {
+private:
+    static std::map<uint8_t, Device *> devices;
+    static constexpr uint8_t BOARD_PINS[] = {
+#ifdef  ARDUINO_ESP8266_ESP01
+            0, 2, 3, 1
+#elif ARDUINO_ESP8266_WEMOS_D1MINI
+            D0, D1, D2, D3, D4, D5, D6, D7, D8, RX, TX
+#endif
+    };
+    static const size_t BOARD_PINS_LEN = sizeof(BOARD_PINS) / sizeof(uint8_t);
+
+    static const bool valid_pin(const uint8_t);
+
+public:
+
+    static Device *put(const uint8_t, const DEVICE_TYPE);
+
+    static Device *get(const uint8_t);
+
+    static bool remove(const uint8_t);
+
+    static const uint8_t *get_pins(size_t *);
+
+    static void get_devices(const DEVICE_TYPE, std::list<Device *> *d);
+};
 
 class Relay : public Device {
 private:
     uint8_t pin, state = 0;
+
 public:
     Relay(const uint8_t);
 
@@ -15,6 +45,8 @@ public:
     void set_state(const bool);
 
     uint8_t get_id();
+
+    DEVICE_TYPE get_type();
 };
 
 #endif //ESP8266_PROJECTS_ROOT_DEVICES_H
