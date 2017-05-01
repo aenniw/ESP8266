11e18c608e64776ff37e4343310c6b88cd3c6b76
Martin Mihálek <aenniw@gmail.com>
2017-05-01 11:48:53 +0200

    Update IOTbase log UI
    - After loading Log page connects to ws
    
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

--- IOTbase/resources/js/log.js
+++ IOTbase/resources/js/log.js
@@ -1,21 +1,20 @@
 var logServer = null;
+serviceOnLoad.set("log", function () {
+    clearLogs();
+    connectLogServer();
+});
 
 function connectLogServer() {
-    if (logServer == null) {
-        logServer = new WebSocket("ws://" + location.host + ":8181");
-        logServer.onmessage = function (event) {
-            var msg = "  " + new Date().toLocaleTimeString() + "  " + event.data + "\n";
-            getE("log-text").innerHTML += msg;
-            console.log(msg);
-        }
-    }
-}
-
-function disconnectLogServer() {
     if (logServer != null) {
         logServer.close();
-        logServer = null;
     }
+    logServer = new WebSocket("ws://" + location.host + ":8181");
+    logServer.onmessage = function (event) {
+        var msg = "  " + new Date().toLocaleTimeString() + "  " + event.data + "\n";
+        getE("log-text").innerHTML += msg;
+        console.log(msg);
+    }
+
 }
 
 function clearLogs() {
