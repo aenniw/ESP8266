128dad0bb69bc2db9a78e72725e3e957534cad0e
Martin Mihálek <aenniw@gmail.com>
2017-05-19 21:21:05 +0200

    Logger functionality update
    - Logger checks if is initialized and if not, all logger activities are ignored
    - Logger uses __LOG_DEBUG__ instead of global __DEBUG_ to enable Serial logging
    
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

--- lib/logger/logger.cpp
+++ lib/logger/logger.cpp
@@ -1,8 +1,9 @@
 #include "logger.h"
 
 WebSocketsServer Log::webSocket(8181);
+static bool initialized = false;
 
-void Log::webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t lenght) {
+void Log::webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t len) {
     switch (type) {
         case WStype_ERROR:
             Log::println("[%u] Error!\n", num);
@@ -19,22 +20,27 @@ void Log::webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t le
             Log::println("[%u] get Text: %s\n", num, payload);
             break;
         case WStype_BIN:
-            Log::println("[%u] get binary lenght: %u\n", num, lenght);
+            Log::println("[%u] get binary lenght: %u\n", num, len);
             break;
     }
 }
 
 
 void Log::init() {
+    if (initialized)
+        return;
     Log::webSocket.begin();
     Log::webSocket.onEvent(webSocketEvent);
-#ifdef __DEBUG__
+#ifdef __LOG_DEBUG__
     Serial.begin(115200);
     Serial.println();
 #endif
+    initialized = true;
 }
 
 void Log::print(const char *format, ...) {
+    if (!initialized)
+        return;
     va_list arg;
     va_start(arg, format);
     char temp[64];
@@ -50,7 +56,7 @@ void Log::print(const char *format, ...) {
         vsnprintf(buffer, len + 1, format, arg);
         va_end(arg);
     }
-#ifdef __DEBUG__
+#ifdef __LOG_DEBUG__
     Serial.print(buffer);
 #endif
     Log::webSocket.broadcastTXT(buffer);
@@ -60,6 +66,8 @@ void Log::print(const char *format, ...) {
 }
 
 void Log::println(const char *format, ...) {
+    if (!initialized)
+        return;
     va_list arg;
     va_start(arg, format);
     char temp[64];
@@ -75,7 +83,7 @@ void Log::println(const char *format, ...) {
         vsnprintf(buffer, len + 1, format, arg);
         va_end(arg);
     }
-#ifdef __DEBUG__
+#ifdef __LOG_DEBUG__
     Serial.println(buffer);
 #endif
     Log::webSocket.broadcastTXT(buffer);
@@ -85,12 +93,16 @@ void Log::println(const char *format, ...) {
 }
 
 void Log::println(const String &msg) {
-#ifdef __DEBUG__
+    if (!initialized)
+        return;
+#ifdef __LOG_DEBUG__
     Serial.println(msg);
 #endif
     Log::webSocket.broadcastTXT(msg.c_str());
 }
 
 void Log::cycle_routine() {
+    if (!initialized)
+        return;
     Log::webSocket.loop();
 }
