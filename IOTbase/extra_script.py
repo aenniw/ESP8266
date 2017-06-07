dbdd0dcda7485dabf37e14636090d3a11b84e4e3
Martin Mihálek <aenniw@gmail.com>
2017-06-07 08:16:26 +0200

    Add check for SPIFFS name lenght
    - Checks if file name len in SPIFFS is lower than 30,
      as this is SPIFFS limit
    
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

--- IOTbase/extra_script.py
+++ IOTbase/extra_script.py
@@ -17,14 +17,26 @@ def cleanup_dir(directory):
             os.remove(root + "/" + name)
 
 
+def valid_ffs_name(ffs_name, ffs_max_len=30):
+    if len(ffs_name) >= ffs_max_len:
+        raise AttributeError("File name is to long for SPIFFS max 30 characters. ", ffs_name)
+
+
 def process_dir(directory):
     for root, dirs, files in os.walk(directory):
         dest_root = "./data/" + "/".join(root.split("/")[2:])
         for name in files:
+            if directory.count("/") == 1:
+                ffs_name = directory[len(directory):]
+            else:
+                ffs_name = directory[directory.find("/", 3):]
+            ffs_name += "/" + name
             if name.endswith((".html", ".css", ".js")):
+                valid_ffs_name(ffs_name, 27)
                 with open(root + "/" + name, 'rb') as f_in, gzip.open(dest_root + "/" + name + ".gz", 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
             elif name.endswith(".json"):
+                valid_ffs_name(ffs_name)
                 with open(root + "/" + name, 'r') as f_in, open(dest_root + "/" + name, 'w') as f_out:
                     while True:
                         c = f_in.read(1)
@@ -34,6 +46,7 @@ def process_dir(directory):
                             continue
                         f_out.write(c)
             else:
+                valid_ffs_name(ffs_name)
                 shutil.copyfile(root + "/" + name, dest_root + "/" + name)
         for dir_to_process in dirs:
             if not dir_to_process.startswith("."):
