2e6a9fdd7729fe01af3fb2539ab692c908f0038c
Martin Mihálek <aenniw@gmail.com>
2017-07-06 21:11:05 +0200

    Add animations for led-strip
    - add 2 animations
    - fix issues with strip UI
    
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

--- lib/service_led_strip/service_led_strip.cpp
+++ lib/service_led_strip/service_led_strip.cpp
@@ -1,6 +1,6 @@
 #include "service_led_strip.h"
 
-#define ANIMATION_BEGIN 0
+#define ANIMATION_INDEX 0
 #define ANIMATION_END 1000
 
 static RgbColor wheel(uint8_t pos) {
@@ -15,28 +15,38 @@ static RgbColor wheel(uint8_t pos) {
     }
 }
 
-void animation_(const AnimationParam &param) {
-
+// TODO: use something more optimized
+static HsbColor wheel(const uint8_t pos, const float b) {
+    HsbColor color = HsbColor(wheel(pos));
+    color.B = b;
+    return color;
 }
 
 void LedStripService::animation_0(const AnimationParam &param) {
+    const uint8_t step = (uint8_t)(param.progress * 255);
     for (uint16_t i = 0; i < led_strip->PixelCount(); i++) {
-        led_strip->SetPixelColor(i, wheel((i + (param.index % 256)) & 255));
+        led_strip->SetPixelColor(i, wheel((uint8_t)(i + step), color.B));
+    }
+    if (param.state == AnimationState_Completed) {
+        animator->RestartAnimation(ANIMATION_INDEX);
     }
 }
 
 void LedStripService::animation_1(const AnimationParam &param) {
+    const uint8_t step = (uint8_t)(param.progress * 255);
     for (uint16_t i = 0; i < led_strip->PixelCount(); i++) {
         led_strip->SetPixelColor(
-                i, wheel(((i * 256 / led_strip->PixelCount()) + (param.index % 256)) & 255));
+                i, wheel((uint8_t)((i * 256 / led_strip->PixelCount()) + step), color.B));
+    }
+    if (param.state == AnimationState_Completed) {
+        animator->RestartAnimation(ANIMATION_INDEX);
     }
 }
 
-LedStripService::LedStripService(const uint16_t len, const uint8_t p) {
-    pin = p;
+LedStripService::LedStripService(const uint16_t len) {
+    animator = new NeoPixelAnimator(ANIMATION_END, 10);
     set_len(len);
     set_color(0, 0, 0);
-    animator = new NeoPixelAnimator(255);
 }
 
 LedStripService::~LedStripService() {
@@ -48,7 +58,7 @@ void LedStripService::set_len(uint16_t len) {
     if (led_strip != NULL) {
         delete led_strip;
     }
-    led_strip = new NeoPixelBus<NeoGrbFeature, NeoEsp8266BitBang800KbpsMethod>(len, pin);
+    led_strip = new NeoPixelBus<NeoGrbFeature, NeoEsp8266Uart800KbpsMethod>(len);
     led_strip->Begin();
 }
 
@@ -56,28 +66,27 @@ uint16_t LedStripService::get_len() const {
     return led_strip->PixelCount();
 }
 
-void LedStripService::set_color(const uint16_t h, const uint8_t s, const uint8_t b) {
-    hue = (uint16_t) (h % 360);
-    saturation = (uint8_t) (s % 101);
-    brightness = (uint8_t) (b % 101);
+void LedStripService::set_color(const uint8_t r, const uint8_t g, const uint8_t b) {
+    color = HsbColor(RgbColor(r, g, b));
     if (SINGLE_COLOR != mode) {
-        set_mode(SINGLE_COLOR);
-    } else {
-        led_strip->ClearTo(HsbColor(hue, saturation / (float) 100, brightness / (float) 100));
+        animator->StopAll();
+        mode = SINGLE_COLOR;
     }
+    led_strip->ClearTo(color);
 }
 
 uint32_t LedStripService::get_color() const {
-    return (uint32_t) ((0xFFFF0000 & hue << 16) | (0x0000FF00 & saturation << 8) | brightness);
+    const RgbColor rgb = RgbColor(color);
+    return (uint32_t)((0x00FF0000 & rgb.R << 16) | (0x0000FF00 & rgb.G << 8) | rgb.B);
 }
 
 void LedStripService::set_brightness(const uint8_t b) {
-    brightness = b;
-    led_strip->ClearTo(HsbColor(hue, saturation / (float) 100, brightness / (float) 100));
+    color = HsbColor(color.H, color.S, (b % 101) / (float) 100);
+    led_strip->ClearTo(color);
 }
 
 uint8_t LedStripService::get_brightness() const {
-    return brightness;
+    return (uint8_t)(color.B * 100);
 }
 
 void LedStripService::set_delay(const uint16_t d) {
@@ -89,17 +98,18 @@ uint16_t LedStripService::get_delay() const {
 }
 
 void LedStripService::set_mode(const LED_STRIP_MODE m) {
+    animator->StopAll();
     switch ((mode = m)) {
         case SINGLE_COLOR:
-            set_color(hue, saturation, brightness);
+            led_strip->ClearTo(color);
             return;
         case ANIMATION_0:
-            animator->StartAnimation(ANIMATION_BEGIN, ANIMATION_END, [this](const AnimationParam &param) {
+            animator->StartAnimation(ANIMATION_INDEX, ANIMATION_END, [this](const AnimationParam &param) {
                 animation_0(param);
             });
             break;
         case ANIMATION_1:
-            animator->StartAnimation(ANIMATION_BEGIN, ANIMATION_END, [this](const AnimationParam &param) {
+            animator->StartAnimation(ANIMATION_INDEX, ANIMATION_END, [this](const AnimationParam &param) {
                 animation_1(param);
             });
             break;
@@ -112,7 +122,7 @@ LED_STRIP_MODE LedStripService::get_mode() const {
 
 void LedStripService::cycle_routine() {
     // Call at least every 10ms
-    if (animator->IsAnimating())
+    if (animator != NULL && animator->IsAnimating())
         animator->UpdateAnimations();
     if (led_strip != NULL && led_strip->CanShow())
         led_strip->Show();
